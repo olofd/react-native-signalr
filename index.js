1d1d2bf593d1f3914a6d07d6a4effdba40cdd8be
Olof Dahlbom <olof.dahlbom@me.com>
2016-03-03 16:45:16 +0100

    Formatting and changing to then for dropped done-api in ES6 promises.

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 let signalRHubConenctionFunc;
 let oldLogger = window.console.debug;
 window.document = {
-  readyState : 'complete'
+  readyState: 'complete'
 };
-if(!window.addEventListener){
-  window.addEventListener = window.addEventListener = () => { };
+if (!window.addEventListener) {
+  window.addEventListener = window.addEventListener = () => {};
 }
 window.navigator.userAgent = "react-native";
 window.jQuery = require('./lib/signalr-jquery-polyfill.js');
 
 module.exports = {
-  setLogger : (logger) => {
-    if(window.console && window.console.debug){
-        window.console.debug("OVERWRITING CONSOLE.DEBUG in react-native-signalr");
-    }else{
-      if(!window.console){
+  setLogger: (logger) => {
+    if (window.console && window.console.debug) {
+      window.console.debug("OVERWRITING CONSOLE.DEBUG in react-native-signalr");
+    } else {
+      if (!window.console) {
         window.console = {};
       }
     }
     window.console.debug = logger;
   },
-  hubConnection : (serverUrl) => {
-    if(!signalRHubConenctionFunc){
-        require('ms-signalr-client');
-        signalRHubConenctionFunc = window.jQuery.hubConnection;
+  hubConnection: (serverUrl, headers) => {
+    if (!signalRHubConenctionFunc) {
+      require('ms-signalr-client');
+      signalRHubConenctionFunc = window.jQuery.hubConnection;
     }
     const protocol = serverUrl.split('//')[0];
     const host = serverUrl.split('//')[1];
     window.location = {
-      protocol : protocol, 
-      host : host
+      protocol: protocol,
+      host: host
     };
     window.document = {
-        createElement : function(){
-           return {
-               protocol : protocol,
-               host : host
-              }
-           }
+      createElement: function() {
+        return {
+          protocol: protocol,
+          host: host
+        }
+      }
     };
+
+    window.jQuery.defaultAjaxHeaders = headers;
+
     return signalRHubConenctionFunc(serverUrl);
   }
-};  
+};
